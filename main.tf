596919bf2663412c55eb58628154661e791967fa
Roberth Strand <me@robstr.dev>
2020-09-17 15:55:40 +0200

    Demo

--- main.tf
+++ main.tf
@@ -1,21 +1,24 @@
-# Remote State
-data "terraform_remote_state" "main" {
-  backend = "remote"
-  config = {
-    organization = "skyarkitektur"
-    workspaces = {
-      name = "demo-terraform-cloud"
-    }
-  }
-}
 # Provider, Azure
 provider "azurerm" {
   version = "=2.20.0"
   features {}
 }
 
-module "demo_module" {
+module "resource_group" {
   source = "./modules/resource_group"
 
   name   = "terraform-cloud-rg"
+}
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = module.resource_group.name
+  address_space       = "10.0.0.0/16"
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  tags = {
+    environment = "demo"
+  }
+
+  depends_on = [module.resource_group]
 }
--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "cloud" {
   name     = var.name
   location = var.location
 }
+
+output "name" {
+  value = azurerm_resource_group.cloud.name
+}
